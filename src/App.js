bf23fab449e30d3d289b89df4ad98c6eb9f60331
Carlos Morales <carlosmora230190@gmail.com>
2023-03-04 06:15:26 -0300

    Se instalo Material UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Navbar from "./components/Navbar/Navbar";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import Footer from "./components/Footer/Footer";
+// import Navbar from "./components/Navbar/Navbar";
+// import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+// import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <>
-      <Navbar/>
+      {/* <Navbar/>
       <ItemListContainer tituloTemporal={"Este Producto pronto estara a la venta"}/>
-      <Footer/>
+      <Footer/> */}
+      <Main/>
       </>
     </div>
   );
