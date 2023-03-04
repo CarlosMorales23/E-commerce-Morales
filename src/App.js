7772502b3a405ed19f0ed41f6b6ea1a26ed5aae3
Carlos Morales <carlosmora230190@gmail.com>
2023-03-04 07:10:37 -0300

    Se limpia app.js para MUI.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import Navbar from "./components/Navbar/Navbar";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <>
-      <Navbar/>
-      <ItemListContainer tituloTemporal={"Este Producto pronto estara a la venta"}/>
-      <Footer/>
+      <Main/>
       </>
     </div>
   );
