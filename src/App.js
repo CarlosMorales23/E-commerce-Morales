9ebb5ee284760330da802c8071cf0536787d0188
Carlos Morales <carlosmora230190@gmail.com>
2023-03-09 00:03:26 -0300

    Termine la clase 2

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@
 // import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 // import Footer from "./components/Footer/Footer";
 
-import ItemListContainer2 from "./components/PracticaSiguiendoAlProfe/Componentes Clase -2/ItemListContainer/ItemListContainer2";
-import Navbar2 from "./components/PracticaSiguiendoAlProfe/Componentes Clase -2/Navbar/Navbar2";
-import ProductCard from "./components/PracticaSiguiendoAlProfe/Componentes Clase -2/ProductCard/ProductCard";
+import Clase2 from "./components/PracticaSiguiendoAlProfe/Componentes Clase -2/Clase2";
 
 function App() {
     return (
         <div className="App">
             <>
                 {/* <Navbar/>
-      <ItemListContainer tituloTemporal={"Este Producto pronto estara a la venta"}/>
-      <Footer/> */}
-                <Navbar2 />
-                <ItemListContainer2 />
-                <ProductCard title="Producto #1" price={200} stock={true} />
-                <ProductCard title="Producto #2"  stock={false} />
-                <ProductCard title="Producto #3" price={660} stock={true} />
+                <ItemListContainer tituloTemporal={"Este Producto pronto estara a la venta"}/>
+                <Footer/> */}
+                <Clase2 />
             </>
         </div>
     );
