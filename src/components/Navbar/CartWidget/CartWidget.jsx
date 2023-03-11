f1f4bb69c75a4e03fb4e69ad06c9d333a50535d6
Carlos Morales <carlosmora230190@gmail.com>
2023-03-11 00:06:56 -0300

    Terminado el Navbar conCSS Basico

--- src/components/Navbar/CartWidget/CartWidget.jsx
+++ src/components/Navbar/CartWidget/CartWidget.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import styles from "./CartWidget.module.css";
 
 const CartWidget = () => {
     return (
-        <div>
-            <h3>1</h3>
-            <ShoppingCartIcon />
+        <div className={styles.containerCartwidget}>
+            <div className={styles.contadorContainer}>
+                <h3 className={styles.contador}>1</h3>
+            </div>
+            <div className={styles.carrito}>
+                <ShoppingCartIcon />
+            </div>
         </div>
     );
 };
