ba2237396d6457800e2401563410d734fda31845
Carlos Morales <carlosmora230190@gmail.com>
2023-03-11 00:21:36 -0300

    Comienzo de ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavbarPres from "./components/Navbar/NavbarPres/NavbarPres";
 
 function App() {
     return (
         <div className="App">
             <NavbarPres />
+            <ItemListContainer/>
         </div>
     );
 }
