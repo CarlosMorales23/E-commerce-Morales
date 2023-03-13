4f2a3626e33b12324f0bf9b925aa7aef21ac26ba
Carlos Morales <carlosmora230190@gmail.com>
2023-03-12 21:35:57 -0300

    Termine Clase MUI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// import CustomNavbar from "./components/CustomNabvar/CustomNavbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavbarPres from "./components/Navbar/NavbarPres/NavbarPres";
 import ProductCard from "./components/ProductCard.jsx/ProductCard";
@@ -5,12 +6,13 @@ import ProductCard from "./components/ProductCard.jsx/ProductCard";
 function App() {
     return (
         <div className="App">
+            {/* <CustomNavbar/> */}
             <NavbarPres />
             <ItemListContainer saludo="Pepito Romero" />
             <ProductCard title="Prodcuto #1" price="100$" descripcion="Este es el mejor producto" stock={true} />
-            {/* <ProductCard title="Prodcuto #1" price="100$" descripcion="Este es el mejor producto" stock={false} />
+            <ProductCard title="Prodcuto #1" price="100$" descripcion="Este es el mejor producto" stock={false} />
             <ProductCard title="Prodcuto #1"  descripcion="Este es el mejor producto" stock={true} />
-            <ProductCard title="Prodcuto #1" price="100$" descripcion="Este es el mejor producto" stock={true} /> */}
+            <ProductCard title="Prodcuto #1" price="100$" descripcion="Este es el mejor producto" stock={true} />
         </div>
     )
 }
